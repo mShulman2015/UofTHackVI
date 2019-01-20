c15b4a72aa0c71086ac36f5548702a3f791b5451
mshulman2015@gmail.com <mshulman2015@gmail.com>
2019-01-20 18:52:42 +0000

    turn off test mode

--- smartCar/smartcar_api_controller.py
+++ smartCar/smartcar_api_controller.py
@@ -14,7 +14,7 @@ client = smartcar.AuthClient(
     client_secret="431cb198-322d-4f4c-a38c-2a2a37d9b19f",
     redirect_uri="https://www.mdshulman.com/smartcar/exchange",
     scope=['read_vehicle_info','read_location', 'control_security', 'control_security:unlock', 'control_security:lock'],
-    test_mode=True,
+    test_mode=False,
 )
 
 def get_token():
